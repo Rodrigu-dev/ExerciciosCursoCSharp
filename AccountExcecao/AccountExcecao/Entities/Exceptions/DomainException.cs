2ad38db3b7beea2e7b299fe0417bfdd11465a9cb
Rodrigu bezerra Coelho <rodrigu.dev@outlook.com>
2021-03-05 17:31:44 -0300

    Segunda atualizacao

---
+++ AccountExcecao/AccountExcecao/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountExcecao.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+            //Construtor que chama o construtor da classe genérica
+        }
+    }
+}
---
+++ AccountExcecao/AccountExcecao/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using AccountExcecao.Entities;
+using AccountExcecao.Entities.Exceptions;
+
+namespace ExcecaoAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("------------------------------------------------------------------");
+            Console.WriteLine("Enter account data ");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial Balance: ");
+            double balance = double.Parse(Console.ReadLine());
+            Console.Write("Withdraw Limit: ");
+            double withdrawLimite = double.Parse(Console.ReadLine());
+
+            Account acc = new Account(number, holder, balance, withdrawLimite);
+
+            Console.WriteLine("------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double amountWithdraw = double.Parse(Console.ReadLine());
+            try
+            {
+                acc.Withdraw(amountWithdraw);
+                Console.WriteLine(acc);
+
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
